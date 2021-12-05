e112b451870bee389661b0ecf1f21f89f1aec079
SkyDoge69 <luc2002penta@gmail.com>
2021-12-05 14:17:55 +0200

    Please

--- src/exercise9/EllipseDemo.java
+++ src/exercise9/EllipseDemo.java
@@ -14,7 +14,7 @@ public class EllipseDemo {
         System.out.println("Ellipse 3: " + e3);
         System.out.println("Ellipse 4: " + e4);
 
-        System.out.println("Ellipse 2 equals ellipse 1: " + e2.equal(e1));
-        System.out.println("Ellipse 2 equals ellipse 3: " + e2.equal(e3));
+        System.out.println("Ellipse 2 equals Ellipse 1: " + e2.equal(e1));
+        System.out.println("Ellipse 2 equals Ellipse 3: " + e2.equal(e3));
     }
 }
